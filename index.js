553ea7283eba64496f4b53014b612d79a3ac228d
Martin Ansty <martin@martinansty.co.uk>
2016-07-17 00:15:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+      cursorColor: '#ecf0f1',
+      foregroundColor: '#2ecc70',
+      backgroundColor: '#002f51',
+      borderColor: 'transparent',
+      colors: [
+            '#2d2d2d',
+            '#b9362a',
+            '#3ab15a',
+            '#ec9e11',
+            '#3e7cba',
+            '#8c34b0',
+            '#34a283',
+            '#bec3c6',
+            '#374960',
+            '#de483c',
+            '#35a251',
+            '#ebc806',
+            '#4a92db',
+            '#964ab6',
+            '#3dbe99',
+            '#ecf0f1'
+          ]
+  });
+}
